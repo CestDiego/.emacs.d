d67300d08497026adb7a73ff74ca1683e58895c5
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2014-04-27 21:22:06 -0500

    Ignore OSX files in dired

--- user-lisp/setup-dired.el
+++ user-lisp/setup-dired.el
@@ -2,4 +2,13 @@
 (setq-default dired-details-hidden-string "")
 (dired-details-install)
 (setq dired-listing-switches "-alGh")
+
+;; Extend dired.
+(require 'dired-x)
+(setq-default dired-omit-files-p t)
+
+; Mac OS X clutter is uninteresting.
+(setq dired-omit-files
+      (concat dired-omit-files "\\|^\\.DS_Store$\\|^__MACOSX$\\|^\\.localized$"))
+
 (provide 'setup-dired)

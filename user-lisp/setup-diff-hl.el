98bf7dff556f00d11ce6acf754f65ee611a0ac4d
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2014-05-03 03:50:57 -0500

    Change diff-hl to the cooler git-gutter

--- user-lisp/setup-diff-hl.el
+++
@@ -1,5 +0,0 @@
-;; diff-hl
-(add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
-(add-hook 'dired-mode-hook 'diff-hl-dired-mode)
-
-(provide 'setup-diff-hl)
---
+++ user-lisp/setup-git-gutter.el
@@ -0,0 +1,6 @@
+(add-hook 'prog-mode-hook 'git-gutter-mode)
+(setq git-gutter:modified-sign " ")
+(setq git-gutter:added-sign " ")
+(setq git-gutter:deleted-sign " ")
+
+(provide 'setup-git-gutter)

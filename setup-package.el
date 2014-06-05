9a56bf46c7e15ff5062fd61c59b5d27c6db2a766
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2014-06-05 12:50:17 -0500

    Forgot to add org url for newest version :(

--- setup-package.el
+++ setup-package.el
@@ -3,7 +3,8 @@
 ;; Add repos
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")))
+        ("melpa" . "http://melpa.milkbox.net/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
 

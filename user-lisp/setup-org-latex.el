334389ab96466b85abb14c2eeb4e23ccff7ea424
Diego Berrocal <cestdiego@gmail.com>
2014-06-17 23:58:30 -0500

    Added eperiodic table mode

--- user-lisp/setup-org-latex.el
+++
@@ -1,8 +0,0 @@
-(require 'org-latex)
-(unless (boundp 'org-export-latex-classes)
-  (setq org-export-latex-classes nil))
-(add-to-list 'org-export-latex-classes
-             '("article"
-               "\\documentclass{article}"
-               ("\\section{%s}" . "\\section*{%s}")))
-(provide 'setup-org-latex.el)

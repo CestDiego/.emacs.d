75fb4ca7a92b2a1e4224461d6f064c2d94f844cf
Diego Berrocal <cestdiego@gmail.com>
2014-11-23 10:16:05 -0500

    Added guide key for C-c C-e in web mode

--- user-lisp/setup-guidekey.el
+++ user-lisp/setup-guidekey.el
@@ -1,5 +1,7 @@
 (require 'guide-key)
 (setq guide-key/guide-key-sequence '("C-x p" "C-x x" "C-c p" "C-h" "C-x r" "C-c RET" "C-x v" "SPC"))
+(add-hook 'web-mode-hook (lambda ()
+                           (push guide-key/guide-key-sequence ("C-c C-e"))))
 (setq guide-key/idle-delay 0.25)
 (setq guide-key/recursive-key-sequence-flag t)
 (guide-key-mode 1)

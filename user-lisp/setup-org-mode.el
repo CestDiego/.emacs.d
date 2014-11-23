554c7346000c4c2908319c78cadd1bf47a4498ea
Diego Berrocal <cestdiego@gmail.com>
2014-11-23 10:16:20 -0500

    now ignoring org-clock-timer

--- user-lisp/setup-org-mode.el
+++ user-lisp/setup-org-mode.el
@@ -51,15 +51,16 @@
 
 
 ;; Org-Notify
-(require 'org-notify)
-(org-notify-start)
-(org-notify-add 'appt
-                '(:time "-1s" :period "20s" :duration 10
-                        :actions (-message -ding))
-                '(:time "15m" :period "2m" :duration 100
-                        :actions -notify)
-                '(:time "2h"  :period "5m" :actions -message)
-                '(:time "3d"  :actions -email))
+;; (require 'org-notify)
+;; (org-notify-start)
+
+;; (org-notify-add 'appt
+;;                 '(:time "-1s" :period "20s" :duration 10
+;;                         :actions (-message -ding))
+;;                 '(:time "15m" :period "2m" :duration 100
+;;                         :actions -notify)
+;;                 '(:time "2h"  :period "5m" :actions -message)
+;;                 '(:time "3d"  :actions -email))
 
 
 ;; Org MIME to Send HTML MAILS!
